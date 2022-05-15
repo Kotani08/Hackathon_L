eb9e005bfe6e82d4f1fc75ba4cd720dab1f2de9a
Kotani08 <20c1020017kt@edu.tech.ac.jp>
2022-05-16 00:39:26 +0900

    磁力の動きを追加

--- Assets/Kotani/MagnetSystem.cs
+++ Assets/Kotani/MagnetSystem.cs
@@ -1,30 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MagnetSystem : MonoBehaviour
 {
-    //tagを見て
-    //同じ極なら周囲いくつ以内なら近づく
-    //違う極なら反発する
+    #region 宣言
     [SerializeField]
-    private GameObject magobj;
+    private GameObject magobj;//自分のオブジェクトを入れる
     [SerializeField]
-    private bool magType;//trueでN極 falseでS極
+    private bool magType;//trueでN極 falseでS極と想定（ここの値を変えると極が変化する）
+    [SerializeField]
+    private float magpaw;//反発するちからor引かれる力が変化する(基本は100です)
+    [SerializeField]
+    private bool staicFlag;//磁力判定を持たせたいが動かしたくないオブジェクトはこれをtrueにする
+
+    ///以下は可視化する必要のない変数
+    private GameObject Target;//相手の位置
+    private Vector2 Mypoj;//自分の位置
+    private float distance;//お互いの距離を把握する
     public bool GetMagType()
     {
-        return magType;//範囲に入った相手がどっちの極かを判断する
+        return magType;//相手の極を判断する時に使うゲッター
     }
-    void OnCollisionEnter2D(Collision2D Collision)
+    #endregion
+
+    #region 磁力の範囲に入っている間にする処理
+    void OnTriggerStay2D(Collider2D collider)
     {
-        //MagnetSystemを持っているオブジェクトが範囲に入ったらそのMagTypeによって分岐
-        switch(Collision.gameObject.GetComponent<MagnetSystem>().GetMagType())
+        //staicFlagが負である かつ magobjがnull（未設定で）ない場合通過
+        if(staicFlag!=true && magobj!=null)
         {
-            //ここによびだす
+            //当たった相手がMagnetSystemを所持していた場合通過
+            if(collider.gameObject.GetComponent<MagnetSystem>())
+            {
+                //相手のgameObjectを変数として所持
+                Target = collider.gameObject;
+
+                //必要な情報を関数MagneticForceにいれてそれっぽい動きをする
+                MagneticForce(magobj.transform.position,
+                              Target.gameObject.transform.position,
+                              collider.gameObject.GetComponent<MagnetSystem>().GetMagType()
+                             );
+            }
         }
     }
-    public void Magnet(bool Type)
-    {
+    #endregion
 
-    }
+    #region ダメな数学＆書き方なのでマネしないように解説無し
+    private void MagneticForce(Vector2 MyObj,Vector2 Target,bool ContactObjType)
+	{
+        Vector2 set;
+        distance = (MyObj - Target).magnitude;
+        //自分の位置をVec2として所持
+        Mypoj = magobj.transform.position;
+        if(magType != ContactObjType)
+        {
+            magpaw=magpaw*-1;
+        }
+		if(MyObj.x <= Target.x)
+        {
+            //地面にめり込まないように要改善
+            //自分の方が左にいる
+            Mypoj.x = Mypoj.x -magpaw*Time.deltaTime*(0.01f*(1000f-distance));
+        }
+        else
+        {
+            //自分の方が右にいる
+            Mypoj.x = Mypoj.x +magpaw*Time.deltaTime*(0.01f*(1000f-distance));
+        }
+        if(MyObj.y <= Target.y)
+        {
+            //自分の方が下にいる
+            Mypoj.y = Mypoj.y -magpaw*Time.deltaTime*(0.01f*(1000f-distance));
+        }
+        else
+        {
+            //自分の方が上にいる
+            Mypoj.y = Mypoj.y +magpaw*Time.deltaTime*(0.01f*(1000f-distance));
+        }
+        set.x=Mypoj.x;
+        set.y=Mypoj.y;
+        magobj.transform.position =set;
+        magpaw=Mathf.Abs(magpaw);//絶対値に戻しておく
+	}
+    #endregion
 }
